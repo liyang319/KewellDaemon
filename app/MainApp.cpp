567186da33c8ef36c4f38843a288a5d4c7f34171
liyang <liyang01@inspiry.cn>
2024-06-25 16:54:05 +0800

    creat project

---
+++ app/MainApp.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <chrono>
+#include <iostream>
+#include <curl/curl.h>
+#include <fstream>
+#include "HttpUtility.h"
+#include "Utility.h"
+
+#include "document.h"
+#include "stringbuffer.h"
+#include "writer.h"
+#include <thread>
+#include <chrono>
+
+#include <openssl/md5.h>
+#include <iomanip>
+#include <sstream>
+
+#include "Base.h"
+#include "Version.h"
+
+using namespace rapidjson;
+// maeusing namespace std;
+
+int main()
+{
+    COUT << "=========Kewell Daemon=============" << VERSION << endl;
+}
